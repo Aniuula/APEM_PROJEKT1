f6d52c9715ab065e683ac6c0e8634010b150de3b
Kubin <jakub.kubin22@gmail.com>
2024-03-07 17:41:09 +0100

    spaces

--- uploading.js
+++ uploading.js
@@ -1,26 +1,26 @@
 document.getElementById('uploadButton').addEventListener('click', function() {
-    document.getElementById('fileInput').click();
+  document.getElementById('fileInput').click();
 });
 
 var listObject = $('[data-role="recordings"]');
 
 document.getElementById('fileInput').addEventListener('change', function() {
 
-    const file = this.files[0];
+  const file = this.files[0];
 
-    if (file) {
+  if (file) {
 
-        var url = URL.createObjectURL(file);
-        var audioObject = $("<audio controls></audio>").attr("src", url);
+    var url = URL.createObjectURL(file);
+    var audioObject = $("<audio controls></audio>").attr("src", url);
 
-        var downloadObject = $("<a>&#9660;</a>")
-            .attr("href", url)
-            .attr("download", new Date().toUTCString() + ".wav");
+    var downloadObject = $("<a>&#9660;</a>")
+        .attr("href", url)
+        .attr("download", new Date().toUTCString() + ".wav");
 
-        var holderObject = $('<div class="row"></div>')
-            .append(audioObject)
-            .append(downloadObject);
+    var holderObject = $('<div class="row"></div>')
+        .append(audioObject)
+        .append(downloadObject);
 
-        listObject.append(holderObject);
-    }
+    listObject.append(holderObject);
+  }
 });
