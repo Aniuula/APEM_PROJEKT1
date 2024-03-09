54311366e3e7f5abdf058e5be41fbb7d423dc960
Aniuula <acelinska372@gmail.com>
2024-03-09 20:21:08 +0100

    Reloading the soundtrack

--- mixing.js
+++ mixing.js
@@ -7,7 +7,6 @@ function getQueryParam(name) {
 }
 
 var listObject = $('[data-role="recordings"]');
-
 var audioSrc;
 
 function displayRecordedAudio() {
@@ -20,25 +19,48 @@ function displayRecordedAudio() {
   }
 }
 
+// Create WaveSurfer instance outside the onload event
+const ws = WaveSurfer.create({
+  container: "#waveform",
+  waveColor: "hotpink",
+  progressColor: "paleturquoise",
+  cursorColor: "#57BAB6",
+  cursorWidth: 4,
+  minPxPerSec: 100,
+});
+
 window.onload = function () {
   displayRecordedAudio();
   console.log(audioSrc);
 
-  const ws = WaveSurfer.create({
-    container: "#waveform",
-    waveColor: "hotpink",
-    progressColor: "paleturquoise",
-    cursorColor: "#57BAB6",
-    cursorWidth: 4,
-    minPxPerSec: 100,
-    url: audioSrc,
-  });
+  // Load audio source when needed
+  if (audioSrc) {
+    ws.load(audioSrc);
+  }
 
   ws.on("interaction", () => {
+    ws.play();
+  });
+
+  // Set up the finish event listener
+  ws.on("finish", () => {
+    // When the audio finishes, seek to the beginning and play again
+    ws.seekTo(0);
     ws.play();
   });
 };
 
 function refreshPage() {
   window.location.href = "index.html";
+}
+
+// Function to play audio again using load()
+function playAgain() {
+  // Load audio source again before playing
+  if (audioSrc) {
+    ws.load(audioSrc);
+  }
+
+  // Play the loaded audio
+  ws.play();
 }
