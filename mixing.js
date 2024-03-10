923f32c99f467302119c6df77c8bc02eeb7eedba
Kubin <jakub.kubin22@gmail.com>
2024-03-10 14:27:27 +0100

    a upload

--- mixing.js
+++ mixing.js
@@ -35,43 +35,4 @@ function displayRecordedAudio() {
   }
 }
 
-// Create WaveSurfer instance outside the onload event
-const ws = WaveSurfer.create({
-  container: "#waveform",
-  waveColor: "hotpink",
-  progressColor: "paleturquoise",
-  cursorColor: "#57BAB6",
-  cursorWidth: 4,
-  minPxPerSec: 100,
-});
-
-window.onload = function () {
-  displayRecordedAudio();
-  console.log(audioSrc);
-
-  if (audioSrc) {
-    ws.load(audioSrc);
-  }
-
-  ws.on("interaction", () => {
-    ws.play();
-  });
-
-  ws.on("finish", () => {
-    ws.seekTo(0);
-    ws.play();
-  });
-};
-
-function refreshPage() {
-  window.location.href = "index.html";
-}
-
-function playAgain() {
-  if (audioSrc) {
-    ws.load(audioSrc);
-  }
-
-  ws.play();
-}
 displayRecordedAudio();
